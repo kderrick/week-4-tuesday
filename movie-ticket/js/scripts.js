e220f575cb9e50a56418590a4b115b2b2f33eeb9
Austin Minnon and Kyle Derrick <whi7ee@gmail.com>
2016-01-26 16:50:48 -0800

    push

--- movie-ticket/js/scripts.js
+++ movie-ticket/js/scripts.js
@@ -3,3 +3,46 @@ function movieTicket(movie, time, age) {
   this.time = time;
   this.age = age;
 }
+
+
+var getPrice = function(movie, time, age) {
+  var price = 5;
+
+    if (movie === "Batman"){
+      price += 5;
+    }else {
+      price += 0;
+    } if (time === "Evening") {
+      price += 5;
+    }else {
+      price += 0;
+    }
+    if (age === "Adult") {
+      price += 5;
+    }else {
+      price += 0;
+    }
+    return price;
+};
+
+
+
+
+movieTicket.prototype.ticketPrice = function() {
+  return "Your " + this.age + " ticket to " + this.movie + " at " + this.time + " will be " + getPrice(this.movie, this.time, this.age);
+};
+// user interface logic
+
+$(document).ready(function() {
+  $("form#new-ticket").submit(function(event) {
+  event.preventDefault();
+
+  var inputtedMovie = $("input#new-movie").val();
+  var inputtedTime = $("input#new-time").val();
+  var inputtedAge = $("input#new-age").val();
+  var newMovieTicket = new movieTicket(inputtedMovie, inputtedTime, inputtedAge);
+
+    $("ul#tickets").append("<li><span class='tickets'>" + newMovieTicket.ticketPrice() + "</span></li>");
+    $('input').val("");
+  });
+});
--- movie-ticket/specs/specs.js
+++ movie-ticket/specs/specs.js
@@ -1,8 +1,19 @@
 describe ("movieTicket", function (){
   it("Will create a new movieTicket with the given specifications", function() {
-    var testTicket = new movieTicket("Batman", "Matinee", "Adult");
+    var testTicket = new movieTicket("Batman", "Evening", "Adult");
     expect(testTicket.movie).to.equal("Batman");
-    expect(testTicket.time).to.equal("Matinee");
+    expect(testTicket.time).to.equal("Evening");
     expect(testTicket.age).to.equal("Adult");
+  });
+
+  it("will add ticket price to all movie tickets", function() {
+    var testTicket = new movieTicket ("Batman", "Evening", "Adult");
+    expect(testTicket.ticketPrice()).to.equal("Your Adult ticket to Batman at Evening will be " + 20);
+  });
+});
+
+describe('getPrice', function() {
+  it("will add 5 for new releases, evening movies, and if adult", function() {
+    expect(getPrice("Batman", "Evening", "Adult")).to.equal(20);
   });
 });
